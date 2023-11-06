24038e71da34dcb133c92eab2029dccfa018bf97
jpres <john.presley.27@gmail.com>
2023-11-05 23:00:21 -0500

    implemented basic functionality, added htmx/bootstrap

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jpres27/directions-server
+
+go 1.21.3
--- main.go
+++ main.go
@@ -2,19 +2,43 @@ package main
 
 import (
 	"fmt"
-	"io"
+	"html/template"
 	"log"
 	"net/http"
+	"time"
 )
+
+type Directions struct {
+	Destination string
+	How         string
+}
 
 func main() {
 	fmt.Println("Server starting...")
-	handler1 := func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "Hello World")
-		io.WriteString(w, r.Method)
+	h1 := func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("index.html"))
+		// TODO: figure out how to connect this with a real database and use that
+		directions := map[string][]Directions{
+			"Directions": {
+				{Destination: "Stacks", How: "Idk"},
+				{Destination: "Study spaces", How: "Idk"},
+				{Destination: "Showers", How: "Idk"},
+			},
+		}
 
+		tmpl.Execute(w, directions)
 	}
-	http.HandleFunc("/", handler1)
+
+	h2 := func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(1 * time.Second)
+		destination := r.PostFormValue("destination")
+		how := r.PostFormValue("how")
+		tmpl := template.Must(template.ParseFiles("index.html"))
+		tmpl.ExecuteTemplate(w, "directions-list-element", Directions{Destination: destination, How: how})
+	}
+
+	http.HandleFunc("/", h1)
+	http.HandleFunc("/submit/", h2)
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
